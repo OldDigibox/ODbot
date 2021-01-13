5514d8e383f1645263452d91658934dfe7c024e2
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-01-13 16:24:07 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 /* MIT License
  *
- *  Copyright (c) 2021 olddigibox
+ *  Copyright (c) 2021 Old Digibox
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
