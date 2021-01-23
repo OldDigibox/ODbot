44555185481c15f61a01422ac5fb9edaa016cd5f
Crithub <thealmightygamer212@gmail.com>
2021-01-23 21:02:25 +0000

    Revert "Add GitHub command"
    This reverts commit c2a4c73b354d8ac7648ae104aeca7d88f5c712d6.
    
    'Revert GitHub command'

--- Commands/github.js
+++
@@ -1,8 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-
-module.exports = async({ client, config }, msg) => msg.reply(`Here is a link to the OldDigibox GitHub organisation. Feel free to check out some of our open source projects!\nhttps://github.com/OldDigibox`);
-module.exports.info = {
-	name: "github",
-	description: "Link to the Old Digibox GitHub organisation,\nwhere you can check out some of our open source projects.",
-	aliases: ["gh"],
-};
